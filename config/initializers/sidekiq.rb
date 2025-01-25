6f9686aeb8f957bbea748e17efdfbc58ba018889
Alexis Ferrere <alexferrere@outlook.fr>
2025-01-25 14:49:08 +0100

    Configure Redis Cloud

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,11 @@
 require 'sidekiq'
 require 'sidekiq-scheduler'
 
+REDIS_URL = 'redis://default:iaHtm561lKMiaCQ7t0aJ3TjIepIQYE11@redis-18932.c3.eu-west-1-1.ec2.redns.redis-cloud.com:18932'|| 'redis://localhost:6379/0'
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+
+  config.redis = { url: REDIS_URL }
 
   # Charge la configuration du scheduler
   if Rails.env.development?
@@ -14,5 +17,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = { url: REDIS_URL }
 end
